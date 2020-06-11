1bb3a45e002b3faf9b09297878c55a0da8cf0b99
VoicefulBread66 <craigwusamuel@yahoo.com.sg>
2020-06-11 12:46:22 +0800

    v3.2.1 hotfix

--- scw.lib.js
+++ scw.lib.js
@@ -1,8 +1,10 @@
 /*
-scw.lib.js v3.2.0 stable
-Changes from v3.1.0
-- Added gen_no(a, b, c) for a random number generator
-- Changed all Math.floor() in random code/password generators to Math.round() for better randomization
+scw.lib.js v3.2.1 hotfix
+Bugfixes
+- Fixed a typo in the timebtw() function which caused the 'd' parameter to not change the result
+Changes from v3.2.0
+- Slightly altered the timebtw() function display messages when c is less than b or b is less than c, depending on mode as stated in d.
+- Rolled back changes to random code generators which changed all Math.floor() to Math.round() after multiple "undefined" were discovered appearing. gen_no(a, b, c) is exempt from this change as it has not negatively affected the function.
 
 Copyright VoicefulBread66, 2019-2020
 */
@@ -303,7 +305,7 @@ function timebtw (a, b, c, d = "uf", e = "n") {
         //Defines the normal time function to be repeated with setInterval()
         var norm = function () {
           //Uses different settings for different modes
-          if (c === "dt") {
+          if (d === "dt") {
             diff = b - c;
           } else {
             diff = c - b;
@@ -327,10 +329,10 @@ function timebtw (a, b, c, d = "uf", e = "n") {
              doe = w + "w " + d + "d " + h + "h " + m + "m " + s + "s";
            } else {
              //Message based on mode
-             if (c === "dt") {
-               doe = "The time this countdown is counting down to has come";
+             if (d === "dt") {
+               doe = "NaN";
              } else {
-               doe = "The time at which the timer starts counting up hasn't even started yet!";
+               doe = "NaN";
                clearInterval(norm, 1000);
              }
            }
@@ -341,7 +343,7 @@ function timebtw (a, b, c, d = "uf", e = "n") {
         var mdiff, mw, mwt, mdwt, md, mdt, mdddt, mh, mht, mdddtht, mm, mmt, mdddthtmt, ms;
         var decimal = function () {
           //Uses different settings for different modes
-          if (c === "dt") {
+          if (d === "dt") {
             mdiff = b - c;
           } else {
             mdiff = c - b;
@@ -366,9 +368,9 @@ function timebtw (a, b, c, d = "uf", e = "n") {
            } else {
              //Message based on mode
              if (c === "dt") {
-               doe = "The time this countdown is counting down to has come";
+               doe = "NaN";
              } else {
-               doe = "The time at which the timer starts counting up hasn't even started yet!";
+               doe = "NaN";
                clearInterval(decimal, 864);
              }
            }
@@ -398,22 +400,22 @@ function hex16(a) {
       //Defines variable 'hd' as an array with the possible values of a hexadecimal 'number'
       var hd = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
       //Variable definition of hexadecimal 'numbers'
-      var hn1 = Math.round(Math.random() * 16);
-      var hn2 = Math.round(Math.random() * 16);
-      var hn3 = Math.round(Math.random() * 16);
-      var hn4 = Math.round(Math.random() * 16);
-      var hn5 = Math.round(Math.random() * 16);
-      var hn6 = Math.round(Math.random() * 16);
-      var hn7 = Math.round(Math.random() * 16);
-      var hn8 = Math.round(Math.random() * 16);
-      var hn9 = Math.round(Math.random() * 16);
-      var hn10 = Math.round(Math.random() * 16);
-      var hn11 = Math.round(Math.random() * 16);
-      var hn12 = Math.round(Math.random() * 16);
-      var hn13 = Math.round(Math.random() * 16);
-      var hn14 = Math.round(Math.random() * 16);
-      var hn15 = Math.round(Math.random() * 16);
-      var hn16 = Math.round(Math.random() * 16);
+      var hn1 = Math.floor(Math.random() * 16);
+      var hn2 = Math.floor(Math.random() * 16);
+      var hn3 = Math.floor(Math.random() * 16);
+      var hn4 = Math.floor(Math.random() * 16);
+      var hn5 = Math.floor(Math.random() * 16);
+      var hn6 = Math.floor(Math.random() * 16);
+      var hn7 = Math.floor(Math.random() * 16);
+      var hn8 = Math.floor(Math.random() * 16);
+      var hn9 = Math.floor(Math.random() * 16);
+      var hn10 = Math.floor(Math.random() * 16);
+      var hn11 = Math.floor(Math.random() * 16);
+      var hn12 = Math.floor(Math.random() * 16);
+      var hn13 = Math.floor(Math.random() * 16);
+      var hn14 = Math.floor(Math.random() * 16);
+      var hn15 = Math.floor(Math.random() * 16);
+      var hn16 = Math.floor(Math.random() * 16);
       //Puts 16 hexademical 'numbers' as a string into HTML
       document.getElementById(a).innerHTML = hd[hn1] + hd[hn2] + hd[hn3] + hd[hn4] + hd[hn5] + hd[hn6] + hd[hn7] + hd[hn8] + hd[hn9] + hd[hn10] + hd[hn11] + hd[hn12] + hd[hn13] + hd[hn14] + hd[hn15] + hd[hn16];
     }
@@ -431,22 +433,22 @@ function hex64_16(a) {
       //Defines variable 'hd' as an array with the possible values of a hexadecimal 'number'
       var hd = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', "$", "."];
       //Variable definition of hexadecimal 'numbers'
-      var hn1 = Math.round(Math.random() * 64);
-      var hn2 = Math.round(Math.random() * 64);
-      var hn3 = Math.round(Math.random() * 64);
-      var hn4 = Math.round(Math.random() * 64);
-      var hn5 = Math.round(Math.random() * 64);
-      var hn6 = Math.round(Math.random() * 64);
-      var hn7 = Math.round(Math.random() * 64);
-      var hn8 = Math.round(Math.random() * 64);
-      var hn9 = Math.round(Math.random() * 64);
-      var hn10 = Math.round(Math.random() * 64);
-      var hn11 = Math.round(Math.random() * 64);
-      var hn12 = Math.round(Math.random() * 64);
-      var hn13 = Math.round(Math.random() * 64);
-      var hn14 = Math.round(Math.random() * 64);
-      var hn15 = Math.round(Math.random() * 64);
-      var hn16 = Math.round(Math.random() * 64);
+      var hn1 = Math.floor(Math.random() * 64);
+      var hn2 = Math.floor(Math.random() * 64);
+      var hn3 = Math.floor(Math.random() * 64);
+      var hn4 = Math.floor(Math.random() * 64);
+      var hn5 = Math.floor(Math.random() * 64);
+      var hn6 = Math.floor(Math.random() * 64);
+      var hn7 = Math.floor(Math.random() * 64);
+      var hn8 = Math.floor(Math.random() * 64);
+      var hn9 = Math.floor(Math.random() * 64);
+      var hn10 = Math.floor(Math.random() * 64);
+      var hn11 = Math.floor(Math.random() * 64);
+      var hn12 = Math.floor(Math.random() * 64);
+      var hn13 = Math.floor(Math.random() * 64);
+      var hn14 = Math.floor(Math.random() * 64);
+      var hn15 = Math.floor(Math.random() * 64);
+      var hn16 = Math.floor(Math.random() * 64);
       //Puts 16 hexademical 'numbers' as a string into HTML
       document.getElementById(a).innerHTML = hd[hn1] + hd[hn2] + hd[hn3] + hd[hn4] + hd[hn5] + hd[hn6] + hd[hn7] + hd[hn8] + hd[hn9] + hd[hn10] + hd[hn11] + hd[hn12] + hd[hn13] + hd[hn14] + hd[hn15] + hd[hn16];
     }
@@ -523,7 +525,7 @@ function time2dec_c(a, b, c) {
         c = new Date().getTime();
       } else {
         //Defines c if value provided in parameter
-        c = new Date(c).getTime();
+        c = new Date(c).getTime()
       }
       //Variable for new Date() function
       if (c === undefined) {
@@ -563,24 +565,24 @@ function pswgen(a) {
 		return "You did not specify an element id for the program";
 	} else {
 		var no = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-		var n1 = Math.round(Math.random() * 10);
-		var n2 = Math.round(Math.random() * 10);
+		var n1 = Math.floor(Math.random() * 10);
+		var n2 = Math.floor(Math.random() * 10);
 		var sp = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
-		var s1 = Math.round(Math.random() * 10);
-		var s2 = Math.round(Math.random() * 10);
+		var s1 = Math.floor(Math.random() * 10);
+		var s2 = Math.floor(Math.random() * 10);
 		var al = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-		var a1 = Math.round(Math.random() * 52);
-		var a2 = Math.round(Math.random() * 52);
-		var a3 = Math.round(Math.random() * 52);
-		var a4 = Math.round(Math.random() * 52);
-		var a5 = Math.round(Math.random() * 52);
-		var a6 = Math.round(Math.random() * 52);
-		var a7 = Math.round(Math.random() * 52);
-		var a8 = Math.round(Math.random() * 52);
-		var a9 = Math.round(Math.random() * 52);
-		var a10 = Math.round(Math.random() * 52);
-		var a11 = Math.round(Math.random() * 52);
-		var a12 = Math.round(Math.random() * 52);
+		var a1 = Math.floor(Math.random() * 52);
+		var a2 = Math.floor(Math.random() * 52);
+		var a3 = Math.floor(Math.random() * 52);
+		var a4 = Math.floor(Math.random() * 52);
+		var a5 = Math.floor(Math.random() * 52);
+		var a6 = Math.floor(Math.random() * 52);
+		var a7 = Math.floor(Math.random() * 52);
+		var a8 = Math.floor(Math.random() * 52);
+		var a9 = Math.floor(Math.random() * 52);
+		var a10 = Math.floor(Math.random() * 52);
+		var a11 = Math.floor(Math.random() * 52);
+		var a12 = Math.floor(Math.random() * 52);
 		//Inserts password into HTML
 		document.getElementById(a).innerHTML = al[a1] + al[a2] + no[n1] + al[a3] + sp[s1] + al[a4] + al[a5] + al[a6] + no[n2] + al[a7] + al[a8] + sp[s2] + al[a9] + al[a10] + al[a11] + al[a12];
 	}
